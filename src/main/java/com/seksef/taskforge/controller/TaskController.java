df749ece3a9d4eb705fbb2a73a175ea6f440a88a
SEKSEF <horri1999hor@gmail.com>
2025-10-17 00:05:18 +0100

    fix: update createTask method to use @RequestBody and modify database URL in docker-compose

--- src/main/java/com/seksef/taskforge/controller/TaskController.java
+++ src/main/java/com/seksef/taskforge/controller/TaskController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -39,7 +38,7 @@ public class TaskController {
     }
 
     @PostMapping
-    public ResponseEntity<Task> createTask(@RequestParam Task task) {
+    public ResponseEntity<Task> createTask(@RequestBody Task task) {
         return ResponseEntity.ok(taskService.createTask(task));
     }
 
